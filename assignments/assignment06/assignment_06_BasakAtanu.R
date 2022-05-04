cc5268e151094401f0ff2aac8864f7029817805e
atanubasak2506 <95464515+atanubasak2506@users.noreply.github.com>
2022-05-04 19:19:57 -0400

    assignment 6 and 7

--- assignments/assignment06/assignment_06_BasakAtanu.R
+++ assignments/assignment06/assignment_06_BasakAtanu.R
@@ -1,29 +1,34 @@
 # Assignment: ASSIGNMENT 6
 # Name: Lastname, Firstname
-# Date: 2010-02-14
+# Date: 2022-05-02
 
 ## Set the working directory to the root of your DSC 520 directory
-setwd("/home/jdoe/Workspaces/dsc520")
+
+setwd("C:\\Users\\atanu\\Documents\\BellevueUniversity_MSDS\\DSC520\\Repository\\dsc520")
 
 ## Load the `data/r4ds/heights.csv` to
-heights_df <- read.csv("data/r4ds/heights.csv")
+heights_df <- read.csv("data\\r4ds\\heights.csv")
 
 ## Load the ggplot2 library
 library(ggplot2)
 
 ## Fit a linear model using the `age` variable as the predictor and `earn` as the outcome
-age_lm <-  ___
+head(heights_df)
+age_lm <-  lm(earn~age, data=heights_df)
 
 ## View the summary of your model using `summary()`
-___
+summary(heights_df)
 
 ## Creating predictions using `predict()`
-age_predict_df <- data.frame(earn = predict(___, ___), age=___)
+
+age_predict_df <- data.frame(earn = predict(age_lm, data.frame(age=heights_df$age)), age=heights_df$age)
+head(age_predict_df)
+
 
 ## Plot the predictions against the original data
-ggplot(data = ___, aes(y = ___, x = ___)) +
+ggplot(data = heights_df, aes(y = earn, x = age)) +
   geom_point(color='blue') +
-  geom_line(color='red',data = ___, aes(y=___, x=___))
+  geom_line(color='red',data = age_predict_df, aes(y=earn, x=age))
 
 mean_earn <- mean(heights_df$earn)
 ## Corrected Sum of Squares Total
@@ -32,33 +37,39 @@ sst <- sum((mean_earn - heights_df$earn)^2)
 ssm <- sum((mean_earn - age_predict_df$earn)^2)
 ## Residuals
 residuals <- heights_df$earn - age_predict_df$earn
+residuals
 ## Sum of Squares for Error
 sse <- sum(residuals^2)
 ## R Squared R^2 = SSM\SST
-r_squared <- ___
-
+summary(age_lm)
+r_squared <- summary(age_lm)$r.squared
+r_squared
 ## Number of observations
-n <- ___
+n <- nrow(heights_df)
 ## Number of regression parameters
 p <- 2
 ## Corrected Degrees of Freedom for Model (p-1)
-dfm <- ___
+dfm <- p-1
 ## Degrees of Freedom for Error (n-p)
-dfe <- ___
+dfe <- n-p
 ## Corrected Degrees of Freedom Total:   DFT = n - 1
-dft <- ___
+dft <- n-1
 
 ## Mean of Squares for Model:   MSM = SSM / DFM
-msm <- ___
+msm <- ssm/dfm
+msm
 ## Mean of Squares for Error:   MSE = SSE / DFE
-mse <- ___
+mse <- sse / dfe
+mse
 ## Mean of Squares Total:   MST = SST / DFT
-mst <- ___
+mst <- sst / dft
+mst
 ## F Statistic F = MSM/MSE
-f_score <- ___
-
+f_score <- msm/mse
+f_score
 ## Adjusted R Squared R2 = 1 - (1 - R2)(n - 1) / (n - p)
-adjusted_r_squared <- ___
-
+adjusted_r_squared <- 1 - (1 - r_squared)*(n - 1) / (n - p)
+adjusted_r_squared
 ## Calculate the p-value from the F distribution
 p_value <- pf(f_score, dfm, dft, lower.tail=F)
+
